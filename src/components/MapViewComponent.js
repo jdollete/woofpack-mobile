0aea8e6bd2ae591cfe2f093ed25bdb892554c20e
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-29 23:06:14 -0400

    Show current location of user

--- src/components/MapViewComponent.js
+++ src/components/MapViewComponent.js
@@ -2,28 +2,81 @@ import React from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet } from 'react-native';
 
-const MapViewComponent = ({ latNum, lngNum }) => {
-  const latNumInt = parseFloat(latNum);
-  const lngNumInt = parseFloat(lngNum);
-  return (
-    <MapView
-      style={styles.map}
-      region={{
-        latitude: latNumInt,
-        longitude: lngNumInt,
-        latitudeDelta: 0.0052,
-        longitudeDelta: 0.0051,
-      }}
-    >
-      <MapView.Marker
-        coordinate={{
+class MapViewComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userPosition: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+  }
+
+  watchID: ?number = null
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = parseFloat(position.coords.latitude);
+      const long = parseFloat(position.coords.longitude);
+
+      this.setState({
+        userPosition: {
+          latitude: lat,
+          longitude: long
+        }
+      });
+
+    },
+    (error) => alert(JSON.stringify(error)),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      const lat = parseFloat(position.coords.latitude);
+      const long = parseFloat(position.coords.longitude);
+
+      this.setState({
+        userPosition: {
+          latitude: lat,
+          longitude: long
+        }
+      });
+    });
+  };
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  render() {
+    const { latNum, lngNum } = this.props;
+    const latNumInt = parseFloat(latNum);
+    const lngNumInt = parseFloat(lngNum);
+
+    return (
+      <MapView
+        style={styles.map}
+        region={{
           latitude: latNumInt,
-          longitude: lngNumInt
+          longitude: lngNumInt,
+          latitudeDelta: 0.0052,
+          longitudeDelta: 0.0051,
         }}
-      />
-    </MapView>
-  );
-};
+      >
+        <MapView.Marker
+          coordinate={{
+            latitude: latNumInt,
+            longitude: lngNumInt
+          }}
+        />
+        <MapView.Marker
+          coordinate={this.state.userPosition}
+        />
+      </MapView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   map: {
