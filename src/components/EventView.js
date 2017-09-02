39c2e2e2e16d3ba15ef807de1108e4ab36d735c0
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-09-02 01:03:02 -0400

    Render event view button depending if it's the owner

--- src/components/EventView.js
+++ src/components/EventView.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
 import { MapViewComponent } from './MapViewComponent';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 
@@ -10,14 +11,64 @@ class EventView extends React.Component {
     super(props);
 
     this.onBackPress = this.onBackPress.bind(this);
+    this.onPressEditEvent = this.onPressEditEvent.bind(this);
+    this.onPressDeleteEvent = this.onPressDeleteEvent.bind(this);
   }
 
   onBackPress() {
     Actions.eventList({ type: 'reset' })
   }
 
+  onPressEditEvent() {
+    alert("Event Edit");
+  }
+
+  onPressDeleteEvent() {
+    Alert.alert(
+      'Delete Event Confirmation',
+      'Are you sure you want to cancel this event?',
+      [
+        {text: 'YES', onPress: () => console.log('OK Pressed')},
+        {text: 'CANCEL'},
+      ],
+      { cancelable: false }
+    )
+  }
+
+  renderFooter(author) {
+    const { currentUser } =firebase.auth();
+    const currentUserId = currentUser.uid
+
+    if (author === currentUserId) {
+      return (
+        <Footer>
+          <FooterTab>
+            <Button full onPress={this.onPressEditEvent}>
+              <Text>Edit</Text>
+            </Button>
+          </FooterTab>
+          <FooterTab>
+            <Button full onPress={this.onPressDeleteEvent}>
+              <Text>Delete</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      );
+    } else {
+      return (
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Join Event</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      );
+    }
+  }
+
   render() {
-    const { eventName, dateString, address, lat, lng } = this.props.event
+    const { eventName, dateString, address, lat, lng, author } = this.props.event
 
     return (
       <Container>
@@ -40,13 +91,7 @@ class EventView extends React.Component {
             />
           </View>
         </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
+        {this.renderFooter(author)}
       </Container>
     );
   }
